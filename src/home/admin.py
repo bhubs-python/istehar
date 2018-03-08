c0fb02f1dff44a33ce4ee3dc26059053e066e3ee
Mubarak <mubarak117136@gmail.com>
2018-03-08 20:46:28 +0600

    create all product post model

--- src/home/admin.py
+++ src/home/admin.py
@@ -5,3 +5,4 @@ from . import models
 
 admin.site.register(models.Catagory)
 admin.site.register(models.SubCatagory)
+admin.site.register(models.Product)
--- src/home/models.py
+++ src/home/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import User
+
+from staff import models as staff_model
+
 
 
 
@@ -21,3 +27,47 @@ class SubCatagory(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
+#product post
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Catagory, on_delete=models.CASCADE, null=True, blank=True)
+    subcategory = models.ForeignKey(SubCatagory, on_delete=models.CASCADE, null=True, blank=True)
+    location = models.ForeignKey(staff_model.Thana, on_delete=None, blank=True, null=True)
+
+    """
+    
+    Application:
+        #get product obj
+        phn = model_name.objects.get(id=1)
+        
+        #assign to product obj
+        Product(product_object=phn)
+    
+    """
+
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+    product_object = GenericForeignKey('content_type', 'object_id')
+
+    photos = models.ImageField(upload_to='product/', default='no-img.jpg', blank=True, null=True)
+
+    codition = models.CharField(max_length=20, null=True, blank=True)
+    brand = models.CharField(max_length=100, null=True, blank=True)
+
+    title = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(max_length=1000, null=True, blank=True)
+
+    model = models.CharField(max_length=100, null=True, blank=True)
+
+    authenticity = models.CharField(max_length=20, null=True, blank=True)
+
+    price = models.FloatField(null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.user.username)
+
+    phone_number = models.CharField(max_length=30, null=True, blank=True)
